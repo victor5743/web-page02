6c0377e79c850de1e14e7001c3507ca5031cead3
Victor <vcs5743@gmail.com>
2020-05-06 12:24:54 -0400

    Sidebar component added
    Roboto font added too

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="bg-gray-900" id="app">
+		<Sidebar />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Sidebar from './components/Sidebar.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		Sidebar,
+	},
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/tailwind.css"></style>
