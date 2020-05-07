50d652e440ef88296e0425d90d93b7e513df83b2
Victor <vcs5743@gmail.com>
2020-05-07 15:54:49 -0400

    Sections added in Content component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,7 +3,7 @@
 		<!-- Main image -->
 		<img class="mb-8 shadow-custom" src="@/assets/slide1.jpg" alt="mandalorian" />
 		<!-- Movies list -->
-		<div class="flex items-center mb-8">
+		<div class="flex items-center mt-8">
 			<div class="w-1/5">
 				<a
 					class="bg-gray-800 p-4 mb-8 hover:bg-dark rounded-lg border border-gray-700 shadow-custom h-24 flex justify-center items-center"
@@ -17,7 +17,7 @@
 					class="bg-gray-800 p-4 mb-8 ml-4 hover:bg-dark rounded-lg border border-gray-700 shadow-custom h-24 flex justify-center items-center"
 					href="#"
 				>
-					<img src="@/assets/pixar_logo.svg" alt="Disney Logo" srcset="" />
+					<img src="@/assets/pixar_logo.svg" alt="Pixar Logo" srcset="" />
 				</a>
 			</div>
 			<div class="w-1/5">
@@ -25,7 +25,7 @@
 					class="bg-gray-800 p-4 mb-8 ml-4 hover:bg-dark rounded-lg border border-gray-700 shadow-custom h-24 flex justify-center items-center"
 					href="#"
 				>
-					<img src="@/assets/marvel_logo.svg" alt="Disney Logo" srcset="" />
+					<img src="@/assets/marvel_logo.svg" alt="Marvel Logo" srcset="" />
 				</a>
 			</div>
 
@@ -34,7 +34,7 @@
 					class="bg-gray-800 p-4 mb-8 ml-4 hover:bg-dark rounded-lg border border-gray-700 shadow-custom h-24 flex justify-center items-center"
 					href="#"
 				>
-					<img src="@/assets/star_wars_logo.svg" alt="Disney Logo" srcset="" />
+					<img src="@/assets/star_wars_logo.svg" alt="Star Wars Logo" srcset="" />
 				</a>
 			</div>
 			<div class="w-1/5">
@@ -42,8 +42,136 @@
 					class="bg-gray-800 p-4 mb-8 ml-4 hover:bg-dark rounded-lg border border-gray-700 shadow-custom h-24 flex justify-center items-center"
 					href="#"
 				>
-					<img src="@/assets/national_geographic_logo.svg" alt="Disney Logo" srcset="" />
+					<img
+						src="@/assets/national_geographic_logo.svg"
+						alt="National Geographic Logo"
+						srcset=""
+					/>
 				</a>
+			</div>
+		</div>
+		<!-- Recommendations -->
+		<div>
+			<h2 class="text-gray-500 mb-1">Recommended For You</h2>
+			<div class="flex items-center">
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/snow_white.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/black_panther.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/zootopia.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/one_strange_rock.jpg" alt="star wars" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Continue Watching -->
+		<div class="mt-8">
+			<h2 class="text-gray-500 mb-1">Continue Watching</h2>
+			<div class="flex items-center">
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/the_sound_of_music.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/high_school_musical.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/cinderella.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/one_strange_rock.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/remember_the_titans.jpg" alt="star wars" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Disney + Originals -->
+		<div class="mt-8">
+			<h2 class="text-gray-500 mb-1">Disney + Originals</h2>
+			<div class="flex items-center">
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/lady_and_the_tramp.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/the_mandalorian.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/high_school_musical.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/forky_asks_a_question.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/marvel_hero_project.jpg" alt="star wars" />
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Trending Now -->
+		<div class="mt-8">
+			<h2 class="text-gray-500 mb-1">Trending Now</h2>
+			<div class="flex items-center">
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/avengers_endgame.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/avengers_infinity_war.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/black_panther.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars" />
+					</a>
+				</div>
+				<div class="shadow-xl mr-6">
+					<a href="#">
+						<img src="@/assets/zootopia.jpg" alt="star wars" />
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
